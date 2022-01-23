826012e08b830efb3bd4f3cfc9fb5f55120d0b82
danil <danilskor2000@mail.ru>
2022-01-24 02:00:42 +0700

    Добавлена пояснительная записка

--- src/LaboratoryBottle/MainClass.cs
+++ src/LaboratoryBottle/MainClass.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Management.Instrumentation;
 using System.Windows.Forms;
+using BottleBuilder;
+using System.Diagnostics;
+using System.IO;
+using BottleParameters;
+using KompasConnector;
+using Microsoft.VisualBasic.Devices;
 
 namespace LaboratoryBottle
 {
@@ -14,6 +21,32 @@ namespace LaboratoryBottle
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+                //StressTesting();
+        }
+
+        public static void StressTesting()
+        {
+            var builder = new Builder();
+            var connector = new Konnector();
+            connector.OpenKompas();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var parameters = new Parameters();
+            parameters.SetDefaultParameters();
+            var streamWriter = new StreamWriter($"log.txt", true);
+            var currentProcess = Process.GetCurrentProcess();
+            var count = 0;
+
+            while (true)
+            {
+                builder.BuildBottle(connector, parameters);
+                var computerInfo = new ComputerInfo();
+                var usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory) *
+                                 0.000000000931322574615478515625;
+                streamWriter.WriteLine(
+                    $"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss\\.fff}\t{usedMemory}");
+                streamWriter.Flush();
+            }
         }
     }
 }
